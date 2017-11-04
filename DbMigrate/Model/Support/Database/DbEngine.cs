b07d5f191169020da100372f9b47cd32d1b61b1d
Arlo Belshee <arlo.belshee@live.com>
2017-11-04 09:06:14 -0700

    Now Tranection only depends on DbEngine.

--- DbMigrate/Model/Support/Database/DbEngine.cs
+++ DbMigrate/Model/Support/Database/DbEngine.cs
@@ -2,8 +2,7 @@
 {
 	public class DbEngine
 	{
-		public static DbEngine SqlServer = new DbEngine(
-			@"if exists(select * from sys.objects where name = '__database_info' and type in ('U'))
+		public static DbEngine SqlServer = new DbEngine("System.Data.SqlClient", @"if exists(select * from sys.objects where name = '__database_info' and type in ('U'))
 begin
 	select top 1 version_number from __database_info;
 end
@@ -12,11 +11,13 @@ begin
 	select -1;
 end");
 
-		private DbEngine(string requestVersionSql)
+		private DbEngine(string providerFactoryName, string requestVersionSql)
 		{
+			ProviderFactoryName = providerFactoryName;
 			RequestVersionSql = requestVersionSql;
 		}
 
-		public string RequestVersionSql { get; }
+		public readonly string ProviderFactoryName;
+		public readonly string RequestVersionSql;
 	}
 }
--- DbMigrate/Model/Support/Database/DbTranection.cs
+++ DbMigrate/Model/Support/Database/DbTranection.cs
@@ -7,16 +7,15 @@ namespace DbMigrate.Model.Support.Database
 {
 	public class DbTranection : ITranection
 	{
-		private const string SqlServerProvider = "System.Data.SqlClient";
 		private const string SqlLiteProvider = "System.Data.SQLite";
 		private DbConnection _connection;
 		private DbTransaction _transaction;
-		private readonly DbProviderFactory _dbTypeToUse;
+		private readonly DbEngine _dbEngine;
 
 		public DbTranection(string connectionString)
 		{
 			ConnectionString = connectionString + "Asynchronous Processing=True;MultipleActiveResultSets=true;";
-			_dbTypeToUse = DbProviderFactories.GetFactory(SqlServerProvider);
+			_dbEngine = DbEngine.SqlServer;
 		}
 
 		public void Dispose()
@@ -63,10 +62,10 @@ namespace DbMigrate.Model.Support.Database
 		private void EnsureIsOpen()
 		{
 			if (IsOpen) return;
-			_connection = _dbTypeToUse.CreateConnection();
+			_connection = DbProviderFactories.GetFactory(_dbEngine.ProviderFactoryName).CreateConnection();
 			if (_connection == null)
 				throw new InvalidOperationException(
-					$"Failed to connect to database using provider {SqlServerProvider} and connection string {ConnectionString}");
+					$"Failed to connect to database using provider {_dbEngine.ProviderFactoryName} and connection string {ConnectionString}");
 			_connection.ConnectionString = ConnectionString;
 			_connection.Open();
 			_transaction = _connection.BeginTransaction(IsolationLevel.Serializable);
