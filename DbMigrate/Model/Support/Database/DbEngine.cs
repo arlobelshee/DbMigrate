b047f981db577863f1717ac2db176f39e8eb24e7
Arlo Belshee <arlo.belshee@live.com>
2017-11-04 18:42:28 -0700

    Use some type from each library, so that the build with recognize the reference is valid and copy it in.

--- DbMigrate/Model/Support/Database/DbEngine.cs
+++ DbMigrate/Model/Support/Database/DbEngine.cs
@@ -1,4 +1,8 @@
-﻿namespace DbMigrate.Model.Support.Database
+﻿using System;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+
+namespace DbMigrate.Model.Support.Database
 {
 	public class DbEngine
 	{
@@ -10,21 +14,25 @@ end
 else
 begin
 	select -1;
-end");
+end", typeof(SqlCommand));
 
 		public static DbEngine SqlLite = new DbEngine("System.Data.SQLite", @"
 select case (select count(*) from sqlite_master where type='table' and name='__database_info' collate nocase)
 when 1 then (select top 1 version_number from __database_info)
 else -1
-end");
+end", typeof(SQLiteCommand));
 
-		private DbEngine(string providerFactoryName, string requestVersionSql)
+		private readonly Type _arbitraryTypeReferenceToEnsureLibraryIsReferencedAndCopied;
+		public readonly string ProviderFactoryName;
+		public readonly string RequestVersionSql;
+
+		private DbEngine(string providerFactoryName, string requestVersionSql,
+			Type arbitraryTypeReferenceToEnsureLibraryIsReferencedAndCopied)
 		{
 			ProviderFactoryName = providerFactoryName;
 			RequestVersionSql = requestVersionSql;
+			_arbitraryTypeReferenceToEnsureLibraryIsReferencedAndCopied =
+				arbitraryTypeReferenceToEnsureLibraryIsReferencedAndCopied;
 		}
-
-		public readonly string ProviderFactoryName;
-		public readonly string RequestVersionSql;
 	}
 }
