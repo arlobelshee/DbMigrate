17ea75af246693957451d27fee5b23f5ab739843
arlobelshee <arlo@belshee.com>
2020-09-02 17:06:35 -0400

    B   Can connect to databases again. Stop using the registry to find DB Factories, and just use the ones that the program is linked against.

--- DbMigrate/Model/Support/Database/DbEngine.cs
+++ DbMigrate/Model/Support/Database/DbEngine.cs
@@ -7,9 +7,9 @@ using DbMigrate.UI;
 
 namespace DbMigrate.Model.Support.Database
 {
-	public class DbEngine
-	{
-		public static DbEngine SqlServer = new DbEngine("System.Data.SqlClient", @"
+    public class DbEngine
+    {
+        public static DbEngine SqlServer = new DbEngine("System.Data.SqlClient", @"
 if exists(select * from sys.objects where name = '__database_info' and type in ('U'))
 begin
 	select top 1 version_number from __database_info;
@@ -17,50 +17,49 @@ end
 else
 begin
 	select -1;
-end", typeof(SqlClientFactory));
+end", SqlClientFactory.Instance);
 
-		public static DbEngine SqlLite = new DbEngine("System.Data.SQLite", @"
+        public static DbEngine SqlLite = new DbEngine("System.Data.SQLite", @"
 select case (select count(*) from sqlite_master where type='table' and name='__database_info' collate nocase)
 when 1 then (select top 1 version_number from __database_info)
 else -1
-end", typeof(SQLiteFactory));
+end", SQLiteFactory.Instance);
 
-		public static DbEngine None = new DbEngine("", null, null);
-		private static readonly Dictionary<string, DbEngine> KnownEngines;
+        public static DbEngine None = new DbEngine("", null, null);
+        private static readonly Dictionary<string, DbEngine> KnownEngines;
 
-		private readonly Type _arbitraryTypeReferenceToEnsureLibraryIsReferencedAndCopied;
-		public readonly string ProviderFactoryName;
-		public readonly string RequestVersionSql;
+        private readonly DbProviderFactory _factory;
+        public readonly string ProviderFactoryName;
+        public readonly string RequestVersionSql;
 
-		static DbEngine()
-		{
-			KnownEngines = new Dictionary<string, DbEngine> {{"sqlite", SqlLite}, {"sqlserver", SqlServer}};
-		}
+        static DbEngine()
+        {
+            KnownEngines = new Dictionary<string, DbEngine> { { "sqlite", SqlLite }, { "sqlserver", SqlServer } };
+        }
 
-		private DbEngine(string providerFactoryName, string requestVersionSql,
-			Type arbitraryTypeReferenceToEnsureLibraryIsReferencedAndCopied)
-		{
-			ProviderFactoryName = providerFactoryName;
-			RequestVersionSql = requestVersionSql;
-			_arbitraryTypeReferenceToEnsureLibraryIsReferencedAndCopied =
-				arbitraryTypeReferenceToEnsureLibraryIsReferencedAndCopied;
-		}
+        private DbEngine(string providerFactoryName, string requestVersionSql,
+            DbProviderFactory factory)
+        {
+            ProviderFactoryName = providerFactoryName;
+            RequestVersionSql = requestVersionSql;
+            _factory = factory;
+        }
 
-		public static string KnownEngineNames =>
-			string.Join(", ", KnownEngines.Keys);
+        public static string KnownEngineNames =>
+            string.Join(", ", KnownEngines.Keys);
 
-		public static DbEngine LookUpByName(string name)
-		{
-			if (!KnownEngines.ContainsKey(name.ToLower()))
-				throw new TerminateProgramWithMessageException(
-					$"I don't know the database engine '{name}'. I only understand how to communicate with {KnownEngineNames}. Please extend me if you want to use that engine.",
-					3);
-			return KnownEngines[name];
-		}
+        public static DbEngine LookUpByName(string name)
+        {
+            if (!KnownEngines.ContainsKey(name.ToLower()))
+                throw new TerminateProgramWithMessageException(
+                    $"I don't know the database engine '{name}'. I only understand how to communicate with {KnownEngineNames}. Please extend me if you want to use that engine.",
+                    3);
+            return KnownEngines[name];
+        }
 
-		public DbConnection CreateConnection()
-		{
-			return DbProviderFactories.GetFactory(ProviderFactoryName).CreateConnection();
-		}
-	}
+        public DbConnection CreateConnection()
+        {
+            return _factory.CreateConnection();
+        }
+    }
 }
